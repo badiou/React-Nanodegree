fcf35c1a5b424d90bb9b9732444d989700dfe2a2
Badiou OURO-BANG'NA <ourobadiou@Badious-MacBook-Air.local>
2025-11-11 10:25:57 +0100

    mise a jour contactApp

--- Contacts App/Starter/src/components/ListContacts.jsx
+++ Contacts App/Starter/src/components/ListContacts.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React from 'react';
+import PropTypes, { array, object } from "prop-types";
+import {useState} from 'react';
 
 const ListContacts = ({contacts, onDeleteContact}) => {
-    console.log(contacts);
-    
+    //console.log(contacts);
+    const [query, setQuery] = useState("");
+
+    // fonction qui s'exacute quand le contenu du champ change
+    const updateQuery=(query)=>{
+      setQuery(query.trim());
+      //console.log(query);
+    }
+
+    const showingContacts = (query) === "" ? 
+    contacts : contacts.filter((contact)=>(contact.name.toLowerCase().includes(query.toLowerCase())));
+
+    //console.log(query); // 👈 placé ici
   return (
-    <div>
+   <div className="list-contacts">
+    <div className="list-contacts-top">
+      <input 
+          type="text" 
+          className="search-contacts" 
+          placeholder='Saisir le nom du contact'
+          value={query}
+          onChange={(e)=>updateQuery(e.target.value)}/>
+    </div>
+     <div>
       <ul className='contact-list'>
+         
         {
-            contacts.map((contact)=>
+         
+            showingContacts.map((contact)=>
                 (
                 <li key={contact.id} className='contact-list-item'>
                     <div className='contact-avatar' 
@@ -26,7 +50,14 @@ const ListContacts = ({contacts, onDeleteContact}) => {
         }
         </ul>
     </div>
+   </div>
   )
 }
+
+
+ListContacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onDeleteContact: PropTypes.func.isRequired,
+};
 
 export default ListContacts
--- L3. State Management/3. Managing State/Starter/src/App.js
+++ L3. State Management/3. Managing State/Starter/src/App.js
@@ -1,12 +1,18 @@
 import corgi from "./corgi.jpg";
 import "./App.css";
+import {useState} from 'react'
 
 const App = () => {
+  const [nombreLike, setNombreLike] = useState(0);
+
+  const countLike=()=>{
+    setNombreLike(prev => prev +1);
+  }
   return (
     <div className="container">
       <h2>Like this photo!</h2>
-      <img src={corgi} alt="Corgi"></img>
-      <p>Amount of likes: </p>
+      <img src={corgi} alt="Corgi" onClick={countLike}></img>
+      <p>Amount of likes: {nombreLike}</p>
     </div>
   );
 };
